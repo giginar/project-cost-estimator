a9fa574b14639aa77e5055a47025bea1dd8aaf99
yigit kucukcinar <yigitkucukcinar@hotmail.com>
2026-01-31 23:16:30 +0300

    init project

---
+++ src/main/java/com/project/costestimator/CostestimatorApplication.java
@@ -0,0 +1,13 @@
+package com.project.costestimator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostestimatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostestimatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/costestimator/CostestimatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.costestimator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostestimatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
